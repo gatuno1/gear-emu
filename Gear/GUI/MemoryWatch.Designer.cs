3996169662f8535db3ca69258f673802ee240e45
Antonio Sanhueza <antonio_sanhueza@hotmail.com>
2015-05-06 01:17:43 -0300

    Memory Watcher changes 1
    +Created Memory watcher as new plugin for watch memory locations.

---
+++ Gear/GUI/MemoryWatch.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Gear.GUI
+{
+    partial class MemoryWatch
+    {
+        /// <summary> 
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar 
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listWatches = new System.Windows.Forms.ListView();
+            this.colHeaderType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeaderSymbol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeaderAddr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeaderValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeaderVisualizer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colHeaderAction = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listWatches
+            // 
+            this.listWatches.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colHeaderType,
+            this.colHeaderSymbol,
+            this.colHeaderAddr,
+            this.colHeaderValue,
+            this.colHeaderVisualizer,
+            this.colHeaderAction});
+            this.listWatches.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listWatches.GridLines = true;
+            this.listWatches.Location = new System.Drawing.Point(0, 0);
+            this.listWatches.MultiSelect = false;
+            this.listWatches.Name = "listWatches";
+            this.listWatches.ShowGroups = false;
+            this.listWatches.ShowItemToolTips = true;
+            this.listWatches.Size = new System.Drawing.Size(150, 150);
+            this.listWatches.TabIndex = 0;
+            this.listWatches.UseCompatibleStateImageBehavior = false;
+            this.listWatches.View = System.Windows.Forms.View.Details;
+            // 
+            // colHeaderType
+            // 
+            this.colHeaderType.Text = "Type";
+            // 
+            // colHeaderSymbol
+            // 
+            this.colHeaderSymbol.Text = "Symbol";
+            // 
+            // colHeaderAddr
+            // 
+            this.colHeaderAddr.Text = "Address";
+            // 
+            // colHeaderValue
+            // 
+            this.colHeaderValue.Text = "Value";
+            // 
+            // colHeaderVisualizer
+            // 
+            this.colHeaderVisualizer.Text = "Vis";
+            // 
+            // colHeaderAction
+            // 
+            this.colHeaderAction.Text = "";
+            // 
+            // MemoryWatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listWatches);
+            this.Name = "MemoryWatch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listWatches;
+        private System.Windows.Forms.ColumnHeader colHeaderType;
+        private System.Windows.Forms.ColumnHeader colHeaderSymbol;
+        private System.Windows.Forms.ColumnHeader colHeaderAddr;
+        private System.Windows.Forms.ColumnHeader colHeaderValue;
+        private System.Windows.Forms.ColumnHeader colHeaderVisualizer;
+        private System.Windows.Forms.ColumnHeader colHeaderAction;
+    }
+}
---
+++ Gear/GUI/MemoryWatch.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using Gear.EmulationCore;
+using Gear.PluginSupport;
+using System.Collections.Generic;
+
+namespace Gear.GUI
+{
+    public partial class MemoryWatch : PluginBase
+    {
+        /// @brief List of watches
+        List<MemWatcher> Watches;
+
+        /// @brief Default Constructor
+        public MemoryWatch(PropellerCPU chip)
+            : base(chip)
+        {
+            Watches = new List<MemWatcher>();
+            InitializeComponent();
+            OnReset();
+        }
+
+        /// @brief Title to be shown in tab.
+        public override string Title
+        {
+            get { return "Watches"; }
+        }
+
+        /// @brief Set the reference to the emulated chip. Occurs once the plugin is loaded.
+        public override void PresentChip()
+        {
+            NotifyOnClock();
+        }
+
+        /// @brief Called every clock tick, if called Chip.NotifyOnClock() in method
+        /// PresentChip() above.
+        public override void OnClock(double time, uint sysCounter)
+        {
+            foreach (MemWatcher watch in Watches)
+            {
+                if (watch.haveToUpdate && (watch.HaveToStop(sysCounter)))
+                    BreakPoint();
+            }
+        }
+
+        /// @brief When repaint event is called, is the opportunity to detect changes
+        public override void Repaint(bool force)
+        {
+            //update each watch to detect changes from this moment
+            foreach (MemWatcher watch in Watches)
+            {
+                watch.UpdateValue();
+                watch.UpdateFrame(Chip.Counter);
+            }
+        }
+
+        /// @brief Called when you press the Reset button, to reset the plugin to a
+        ///initial state.
+        public override void OnReset()
+        {
+
+        }
+
+        public class MemWatcher
+        {
+            public enum StopMode
+            {
+                none = 0,       //don't stop on value changes
+                anyChange,      //stop when a value change is detected
+                equalValue      //stop when a specific value is detected
+            }
+
+            private PropellerCPU _chip; //to access main memory
+            private Cog _cog;           //to access cog memory
+
+            public bool IsCogBased { get; set; }
+
+            public string Name { get; set; }    //name to show
+            private uint _addr;
+            public uint Addr
+            {
+                get { return _addr; }
+                set
+                {
+                    if (IsCogBased)
+                    {
+                        if (ValidCogAddr(value))
+                            _addr = value;
+                        else
+                            throw new Exception(string.Format(
+                                "Value '{0:N}'(0x{1:X}) is not valid for Cog Address.",
+                                value, value));
+                    }
+                    else
+                    {
+                        if (ValidHubAddr(value))
+                            _addr = value;
+                        else
+                            throw new Exception(string.Format(
+                                "Value '{0:N}'(0x{1:X}) is not valid for Main memory Address.",
+                                value, value));
+                    }
+                }
+            }
+
+            public uint Value { get; private set; }
+            private uint ComparedValue;
+            private uint LastCounter;
+
+            private StopMode _mode;
+            public StopMode Mode
+            {
+                get { return _mode; }
+                set
+                {
+                    _mode = value;
+                    switch (value)
+                    {
+                        case StopMode.none:
+                            haveToUpdate = false;
+                            break;
+                        case StopMode.anyChange:
+                        case StopMode.equalValue:
+                            haveToUpdate = true;
+                            break;
+                    }
+                }
+            }
+
+            public bool isStoppedBy { get; private set; }
+
+            public void SetComparedValue(uint val)
+            {
+                ComparedValue = val;
+            }
+
+            private uint UpdatedValue()
+            {
+                return Value = (IsCogBased) ?
+                    _cog.ReadLong(_addr) :
+                    _chip.ReadLong(_addr);
+            }
+
+            public void UpdateValue()
+            {
+                UpdatedValue();
+            }
+
+            public bool UpdateFrame(uint counter)
+            {
+                if (counter != LastCounter)
+                {
+                    LastCounter = counter;
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            private bool _mustUpdate;
+            public bool haveToUpdate
+            {
+                get
+                {
+                    if (_mustUpdate)
+                        UpdatedValue();
+                    return _mustUpdate;
+                }
+                private set { _mustUpdate = value; }
+            }
+
+            public bool HaveToStop(uint counter)
+            {
+                bool retVal = false;
+                if (counter != LastCounter)
+                {
+                    switch (Mode)
+                    {
+                        case StopMode.none:
+                            break;
+                        case StopMode.anyChange:
+                            if (retVal = (UpdatedValue() != ComparedValue))
+                                isStoppedBy = true;
+                            break;
+                        case StopMode.equalValue:
+                            if (retVal = (UpdatedValue() == ComparedValue))
+                                isStoppedBy = true;
+                            break;
+                    }
+                }
+                return retVal;
+            }
+
+            /// @brief Create a new Watch for a %Cog memory location.
+            /// @param cog Reference to %Cog to inspect its memory.
+            /// @param name Name to asociate the Watch.
+            /// @param address Memory address to inspect.
+            public MemWatcher(Cog cog, string name, uint address)
+            {
+                IsCogBased = true;
+                _chip = null;
+                _cog = cog;
+                Addr = address; //this assigment validate the value or throw an exception
+                //CogSpecialAddress tmp;
+                //if (Enum.IsDefined(typeof(CogSpecialAddress), Addr) &&
+                //    Enum.TryParse(Addr, out tmp))
+                //{
+                //    specialAddr = tmp;
+                //    Name = specialAddr.ToString();
+                //}
+                Name = name;
+                if (string.IsNullOrEmpty(Name))
+                    Name = name;
+                Value = cog.ReadLong(Addr);
+                ComparedValue = 0;
+                isStoppedBy = false;
+                Mode = StopMode.none;
+            }
+
+            /// @brief Create a new Watch for a Main memory location.
+            /// @param chip Reference to %PropellerCPU to inspect its memory.
+            /// @param name Name to asociate the Watch.
+            /// @param address Memory address to inspect.
+            public MemWatcher(PropellerCPU chip, string name, uint address)
+            {
+                IsCogBased = false;
+                _chip = chip;
+                _cog = null;
+                Addr = address; //this assigment validate the value or throw an exception
+                Name = name;
+                Value = chip.ReadLong(Addr);
+                ComparedValue = 0;
+                isStoppedBy = false;
+                Mode = StopMode.none;
+            }
+
+            private bool ValidCogAddr(uint address)
+            {
+                return (address < Cog.TOTAL_COG_MEMORY);
+            }
+
+            private bool ValidHubAddr(uint address)
+            {
+                return (address < PropellerCPU.TOTAL_RAM);
+            }
+        }
+
+    }
+}
